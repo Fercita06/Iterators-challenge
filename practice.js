f8c938f1cdaf8606a9b07f475b81d4e07b4aea08
suarezfer1712 <suarezfer1712@gmail.com>
2022-06-01 10:28:01 +0100

    first iterators challenge

---
+++ practice.js
@@ -0,0 +1,17 @@
+
+
+const capitalizedNames= (nameArr) => {
+  const newArr = nameArr.map ((name)=> name.upperCase() + nameArr.slice(1));  return newArr;
+}
+
+
+const removeVowels =(vowel) => {
+  const wordWithoutVowels = vowel.filter
+}
+
+
+
+const removeFromArr = (string, array) => {
+    const filteredArr = stringArr.filter(newstring => newstring != string)
+    return filteredArr
+}
