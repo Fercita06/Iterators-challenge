4bf5b63711b12e0f3362b86cdade26b3572982fe
suarezfer1712 <suarezfer1712@gmail.com>
2022-06-01 11:29:22 +0100

    first exercises

--- practice.js
+++ practice.js
@@ -1,17 +1,23 @@
-
+const nameArray = ["Charlie","Rob"]
 
 const capitalizedNames= (nameArr) => {
-  const newArr = nameArr.map ((name)=> name.upperCase() + nameArr.slice(1));  return newArr;
+  const newArr = nameArr.map ((name)=> name.upperCase() + nameArr.slice(1));  
+  return newArr;
 }
 
+const letters = ["f","e","r","n","a","n","d","a"]
 
-const removeVowels =(vowel) => {
-  const wordWithoutVowels = vowel.filter
+const removeVowels = (vowelArr) => {
+  const vowels = ["a","e","i","o","u"];
+  return vowelArr.filter((letter) => !vowels.includes(letter))
+};
+
+const numbers = [25, 50, 175, 50];
+
+const mean = (numArr) => {
+  return numbArr.reduce((total, current) => total +- current, 0) / numArr.length;
+  
 }
 
+console.log (mean)
 
-
-const removeFromArr = (string, array) => {
-    const filteredArr = stringArr.filter(newstring => newstring != string)
-    return filteredArr
-}
